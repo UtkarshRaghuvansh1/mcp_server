580509c5cef7d6093056076baffc5eb8bb90b8b0
Utkarsh Raghuvanshi 30 <utkarsh.raghuvanshi@gehelathcare.com>
2026-03-24 16:36:49 +0530

    MCP server for read and edit docs

--- cli_project/mcp_server.py
+++ cli_project/mcp_server.py
@@ -1,4 +1,5 @@
 from mcp.server.fastmcp import FastMCP
+from pydantic import Field
 
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
 
@@ -13,7 +14,30 @@ docs = {
 }
 
 # TODO: Write a tool to read a doc
+@mcp.tool(
+    name = 'read_doc',
+    description = 'Read the contents of a document.',
+)
+def read_doc(doc_id: str = Field(description="The ID of the document to read")):
+    if doc_id in docs:
+        return docs[doc_id]
+    else:        
+        return "Document not found."
 # TODO: Write a tool to edit a doc
+@mcp.tool(
+    name = 'edit_doc',
+    description = 'Edit the contents of a document.',
+)
+def edit_doc(doc_id: str = Field(description="The ID of the document to edit"), new_content: str = Field(description="The new content for the document"), old_content: str = Field(description="The old content of the document")):
+    if doc_id in docs:
+        if docs[doc_id] == old_content:
+            docs[doc_id] = new_content
+            return "Document updated successfully."
+        else:
+            return "Old content does not match."
+    else:
+        return "Document not found."
+
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
